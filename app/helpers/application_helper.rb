d0db2bd9dc1423a66b5336225e726910ff1f1ee5
Petro <petro.yakubiv@gmail.com>
2018-07-04 21:07:40 +0300

    add popular

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def active_if?(conditions)
+    active_conditions = {
+      action: action_name == conditions[:action],
+      controller: controller_name == conditions[:controller],
+      fullpath: request.fullpath =~ conditions[:fullpath],
+      starts_with: fullpath_starts_with?(conditions[:starts_with].to_s)
+    }.select{ |k,_| conditions.keys.include? k }.values
+    return 'active' if active_conditions.all?
+  end
+
+  def fullpath_starts_with?(path)
+    request.fullpath =~ /^\/#{Regexp.quote(path)}/
+  end
 end
