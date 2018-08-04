d23e6cc0345e2c6ca3f868f0ac69108dbfeebe14
Petro <petro.yakubiv@gmail.com>
2018-08-04 14:17:55 +0300

    simple filters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
-  def intex
-    @posts = Post.all
+  def index
+    @posts = Post.send(filter_options)
+    @posts = Post.tagged_with(params[:tag]) unless params[:tag].blank?
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+
+  def filter_options
+    return params[:category] unless params[:category].blank?
+    'all'
   end
 end
