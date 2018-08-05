3633434f82f8362de9aefb04a40d17469583848d
Petro <petro.yakubiv@gmail.com>
2018-08-05 12:21:24 +0300

    cahnge

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
 
-  default_scope { order(created_at: :asc) }
+  default_scope { order(created_at: :desc) }
 
   def to_param
     url_alies
